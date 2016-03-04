70280bdaf9df29731724c1eb2ffc72ccbe4b90b1
Joe Chasinga <jo.chasinga@gmail.com>
2016-03-04 17:54:44 -0500

    Implement basic admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,16 @@
-from app import app, models
+from app import app, db
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from app.models import User, Stage, Question, Candidate
 
 admin = Admin(app, name="Presidential-X", template_mode="bootstrap3")
+
+class StageAdmin(ModelView):
+        inline_models = (Question,)                                                                                                       
+admin.add_view(ModelView(User, db.session))
+admin.add_view(StageAdmin(Stage, db.session))
+admin.add_view(ModelView(Question, db.session))
+admin.add_view(ModelView(Candidate, db.session))
 
 # Add admin views here
 
