4237d5cb9d430c71089e3fe5965e5d83af9f97df
Joe Chasinga <jo.chasinga@gmail.com>
2016-03-04 01:12:09 -0500

    Add db creation and drop

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.log import enable_pretty_logging
-from app import app
+from app import app, db
+from app.models import User
 
 enable_pretty_logging()
 
@@ -16,6 +17,11 @@ if __name__ == '__main__':
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(PORT)
     ioloop = tornado.ioloop.IOLoop().instance()
+
+    if User.query.all():
+        db.drop_all()
+        
+    db.create_all()
 
     print("Server listening at 127.0.0.1:{}".format(PORT))
     ioloop.start()
